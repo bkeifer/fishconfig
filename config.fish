dc4c4eed7fa1b652ad7a7fd814cb0386e7113f1a
Brian Keifer <keiferb@lafayette.edu>
2021-12-15 20:38:37 -0500

    Add old ZSH aliases

--- config.fish
+++ config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U tide_os_icon 
     
+    alias ipcalc="ipcalc -n"
+    alias python="python3"
+    alias digs="dig +short"
+    alias whoisa="whois -h whois.arin.net"
+
     function xaws
         set -e AWS_ACCESS_KEY_ID
         set -e AWS_SECRET_ACCESS_KEY
