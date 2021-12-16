1447215f0c33db1150ddc940821c400cd81f5151
Brian Keifer <keiferb@lafayette.edu>
2021-12-15 21:08:33 -0500

    Prompt customization

--- config.fish
+++ config.fish
@@ -1,7 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U tide_os_icon 
-    
+    set -U tide_left_prompt_items 'os'  'pwd'  'aws_account'  'git'  'newline'  'character'
+
+    set -U tide_time_color ff8000
+    set -U tide_time_bg_color 000000
+    set -U tide_os_color 000000
+    set -U tide_os_bg_color FF8000
+
     alias ipcalc="ipcalc -n"
     alias python="python3"
     alias digs="dig +short"
