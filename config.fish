cc2f44740f180c3f16f9ee7bf452549b2d906f0e
Brian Keifer <keiferb@lafayette.edu>
2021-12-15 21:32:17 -0500

    More customization

--- config.fish
+++ config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -U tide_os_icon 
     set -U tide_left_prompt_items 'os'  'pwd'  'aws_account'  'git'  'newline'  'character'
 
-    set -U tide_time_color ff8000
-    set -U tide_time_bg_color 000000
+    set -U tide_git_icon 
+
+    set -U tide_os_icon 
+    # set -U tide_os_icon 
     set -U tide_os_color 000000
     set -U tide_os_bg_color FF8000
+
+    set -U tide_pwd_bg_color 124382
+    set -e tide_pwd_icon
+
+    set -U tide_time_color ff8000
+    set -U tide_time_bg_color 222222
 
     alias ipcalc="ipcalc -n"
     alias python="python3"
